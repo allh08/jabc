484c2b26f9c5f03d8f2964d296c2e980103cf2d5
Allen Hu <allen.hu@sap.com>
2020-07-16 13:39:37 -0700

    Updates

---
+++ precache-manifest.dee0cf995689cb31ab3ac879e95fb78f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d52d3b91ac585c54f1c259b7440536",
+    "url": "/jabc/index.html"
+  },
+  {
+    "revision": "1453faa0359e92ef53f1",
+    "url": "/jabc/static/js/2.b88edf4f.chunk.js"
+  },
+  {
+    "revision": "ae1f8eb1ceed59913611",
+    "url": "/jabc/static/js/main.0bc6106f.chunk.js"
+  },
+  {
+    "revision": "dbfc27dd4dd1325bfeac",
+    "url": "/jabc/static/js/runtime-main.5a74851e.js"
+  }
+]);
